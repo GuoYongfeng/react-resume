cfd68d515658552105c880c24fe27183b4f58137
guoyff <guoyff@yonyou.com>
2017-02-21 10:30:44 +0800

    lesson18

--- src/containers/Project/index.js
+++ src/containers/Project/index.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react'
-
-class Project extends Component {
-  render(){
-    return (
-      <div>
-        <h1> Project Me~~!!</h1>
-      </div>
-    )
-  }
+import React from 'react'
+import { Timeline, Modal } from 'antd'
+import TimelineItem from './TimelineItem'
+import { getRepos } from '../../api/fetchGithubMsg'
+import './index.css'
+export default class Project extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { repos: [] }
+    }
+    setReposData( username ) {
+      getRepos( username )
+        .then( info => this.setState({ repos: info.data }))
+    }
+    componentWillMount(){
+      this.setReposData( "guoyongfeng" )
+    }
+    render() {
+      let nodes = this.state.repos.map( items => (<TimelineItem key={items.id}
+          name={items.name}
+          description={items.description}
+          html_url={items.html_url} />
+      ) );
+      return (
+          <div className="project">
+              <h1 className="project-title">项目经验</h1>
+              <div className="project-content">
+                  <Timeline pending={<a href="https://github.com/GuoYongfeng" target="_blank" className="item-title">更多项目内容尽在github（点击进入）</a>}>
+                    { nodes }
+                  </Timeline>
+              </div>
+          </div>
+      )
+    }
 }
-
-export default Project
